9edd71ecef350fcaad8023409cd4b1f8af6a4003
Sara-Jain <Sara_Jain@mckinsey.com>
2022-03-29 13:00:05 +0530

    feat: add table to db

---
+++ migrations/20220329072318-create-banks.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('banks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      BANK: {
+        type: Sequelize.STRING
+      },
+      IFSC: {
+        type: Sequelize.STRING
+      },
+      BRANCH: {
+        type: Sequelize.STRING
+      },
+      ADDRESS: {
+        type: Sequelize.STRING
+      },
+      CITY1: {
+        type: Sequelize.STRING
+      },
+      CITY2: {
+        type: Sequelize.STRING
+      },
+      STATE: {
+        type: Sequelize.STRING
+      },
+      STD_CODE: {
+        type: Sequelize.FLOAT
+      },
+      PHONE: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('banks');
+  }
+};
